91febd3b87097d2186993f82e0190e07990ab485
Aniket Chitransh <ani.chitransh@gmail.com>
2023-12-15 14:36:13 +0530

    Added useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { NewTodoForm } from './NewTodoForm';
 import { TodoList } from './TodoList';
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMs", JSON.stringify(todos))
+  }, [todos])
 
   function addTodo(title) {
     setTodos(currentTodos => {
