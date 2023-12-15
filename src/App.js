d4a3d7a88c3e6bd0411f698a111a893108ec3fed
Aniket Chitransh <ani.chitransh@gmail.com>
2023-12-15 12:21:13 +0530

    Created the UI for the app with JSX

--- src/App.js
+++ src/App.js
@@ -2,7 +2,32 @@ import './App.css';
 
 function App() {
   return (
-    <h1>App</h1>
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor='item'>New Item</label>
+          <input type="text" id='item' />
+          <button className="btn">Add</button>
+        </div>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
   );
 }
 
